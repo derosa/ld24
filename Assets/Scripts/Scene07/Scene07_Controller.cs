26146d9cc30003fc4663363a1f9eba0beaace836
David Erosa <soulnet@gmail.com>
2012-08-26 17:55:50 +0200

    Backup 1345996550

--- Assets/Scripts/Scene07/Scene07_Controller.cs
+++ Assets/Scripts/Scene07/Scene07_Controller.cs
@@ -4,35 +4,29 @@ using System.Collections;
 public class Scene07_Controller : MonoBehaviour {
 	public int nextLevel = 0;
 	public GameObject exitDoor;
-	private bool _monitorPlayerDoorDistance = false;
+	private bool canOpenDoor = false;
 	
 	
-	void Start () {
-	
-	}
-	
-	void Update ()
-	{
-		if (_monitorPlayerDoorDistance) {
-			Vector3 player = GameObject.FindGameObjectWithTag ("Player").transform.position;
-			if (Vector3.Distance (exitDoor.transform.position, player) < 10F) {
-			exitDoor.SendMessage ("CanEnter", true);	
-			}
-		}
-		
-	}	
 	
 	public void LetterGrabbed ()
 	{
-		_monitorPlayerDoorDistance = true;
+		canOpenDoor = true;
 	
 	}
 	
 	public void DoorEntered ()
 	{
 		Debug.Log ("Fin del juego");
-		Application.LoadLevel(nextLevel);
+		Application.LoadLevel (nextLevel);
 	}
+	
+	public void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag ("Player") && canOpenDoor) {
+			exitDoor.SendMessage ("CanEnter", true);	
+		}
+	}
+	
 	
 	
 }
